24b22ade9d690245be62afa38008c1d869a53422
Fabjo Plaka <fabjo.plaka@moveoalbania.com>
2023-10-27 16:52:44 +0200

    get users

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Sidebar } from "../components/UISidebar";
+import { getUsers } from "../actions/getUsers";
+import UserList from "./components/UserList";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 const Layout: React.FC<LayoutProps> = async ({ children }) => {
+  const users = await getUsers();
+
   return (
     <Sidebar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <UserList items={users} />
+        {children}
+      </div>
     </Sidebar>
   );
 };
